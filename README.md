cc17d144d9db3c0dce3b02054f880f604183871b
Mia Nguyen <nguyendanlinh9122000@gmail.com>
2023-04-19 19:52:08 -0700

    level 3: authentication using md5 hash

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # Authentication-Secrets
-
-you can use git log to see all the commits:
+6 levels of authentication:
 
         Level 6 - Google OAuth 2.0 Authentication
         
@@ -12,12 +11,12 @@ you can use git log to see all the commits:
 
 
         Level 2 - Encryption
-        
+        commit 210a0a7693d11ef8f9213c6992c047ecc3f6e850
 
         Level 1 - Store username and password into mongoDB to check
         commit 7ca8f8b04e958c5841cdca3ec8d9604d17293a5a
 
-To see the code after Level 1, simply type this into the terminal:
-        git checkout 7ca8f8b04e958c5841cdca3ec8d9604d17293a5a .
+To see the code after Level 1, simply type this into the terminal: 
+git checkout 7ca8f8b04e958c5841cdca3ec8d9604d17293a5a .
 
 
--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+// const encrypt = require("mongoose-encryption"); //level 2 
+const md5 = require("md5"); //level 3
 
 const app = express();
 
@@ -27,7 +28,8 @@ const userSchema = new mongoose.Schema({
   password: String
 });
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET_STRING, encryptedFields: ['password'] });  //plugin the secret string into schema
+// plugin the secret string into schema (level 2)
+// userSchema.plugin(encrypt, { secret: process.env.SECRET_STRING, encryptedFields: ['password'] });  
 
 const User = new mongoose.model("User", userSchema);
 
@@ -48,8 +50,10 @@ app.get("/register", function(req, res){
 
 app.post("/register", async(req, res)=>{
   const newUser =  new User({
+    // email: req.body.username,
+    // password: req.body.password
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password) //change password into an irreversible string using md5 hash
   });
   try{
     await newUser.save();
@@ -60,7 +64,7 @@ app.post("/register", async(req, res)=>{
 
 app.post("/login", async(req, res)=>{
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);  //change is into the md5 string and compared with stored md5 string in database
   try{
     const foundUser = await User.findOne({email: username})
     if(foundUser) {
